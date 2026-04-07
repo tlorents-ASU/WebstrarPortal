b9facd45cabf98909722745adad2409b6f95bd35
tlorents-ASU <tlorents@asu.edu>
2026-04-07 15:35:14 -0700

    redirect to webstrarportal.fulton.asu.edu/portal

--- WebstrarPortal/Controllers/HomeController.cs
+++ WebstrarPortal/Controllers/HomeController.cs
@@ -7,10 +7,6 @@ public class HomeController : Controller
     [HttpGet("/")]
     public IActionResult Index()
     {
-        // Keep it simple for now: just prove the app is alive
-        return Content(
-            "WebstrarPortal is running. Try /portal/deploy/test?asurite=tlorents&page=Page0",
-            "text/plain"
-        );
+        return Redirect("/portal");
     }
 }
